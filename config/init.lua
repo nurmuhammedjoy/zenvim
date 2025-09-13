f9b2dcf49840d1d6a1696dfd3c0031d297f3358f
Muhammad <forminecraftnurmuhammadjoy@gmail.com>
2025-09-13 20:01:07 +0600

    add configuration

---
+++ config/init.lua
@@ -0,0 +1,409 @@
+
+-- Author: Nur Muhammed Joy
+-- Date: 2025-9-13
+-- Description: A comprehensive Neovim configuration optimized
+-- for Termux on Android.
+-- Features: include LSP support, autocompletion,
+-- file explorer, fuzzy finder, GitHub Copilot integration,
+-- and a modern UI with Gruvbox theme.
+-- 
+
+-- Disable unused providers for performance
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+vim.opt.updatetime = 100
+vim.opt.timeoutlen = 300
+vim.opt.ttimeoutlen = 0
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 200
+vim.opt.redrawtime = 1500
+vim.opt.fillchars = { eob = " " }
+-- Basic settings
+
+vim.opt.encoding = "utf-8"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.config/nvim/undodir")
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin configuration
+require("lazy").setup({
+  -- Gruvbox theme
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = false,
+        underline = false,
+        bold = false,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = false,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = false,
+        contrast = "hard",
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
+      })
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+
+  -- File explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+        window = {
+          position = "left",
+          width = 30,
+        },
+        filesystem = {
+          filtered_items = {
+            visible = false,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+          use_libuv_file_watcher = false,
+        },
+      })
+    end
+  },
+
+  -- Fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          file_ignore_patterns = { 
+            "node_modules/.*", 
+            "%.git/.*", 
+            "%.DS_Store",
+            "package%-lock%.json"
+          },
+          layout_strategy = "horizontal",
+          layout_config = {
+            width = 0.87,
+            height = 0.80,
+          },
+        },
+      })
+    end
+  },
+
+  -- GitHub Copilot
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      
+      vim.g.copilot_filetypes = {
+        ["*"] = false,
+        ["javascript"] = true,
+        ["typescript"] = true,
+        ["html"] = true,
+        ["css"] = true,
+        ["scss"] = true,
+        ["json"] = true,
+        ["yaml"] = true,
+        ["markdown"] = true,
+        ["lua"] = true,
+        ["python"] = true,
+        ["vue"] = true,
+        ["php"] = true,
+      }
+    end
+  },
+
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "html",
+          "cssls",
+          "ts_ls",
+          "jsonls",
+          "emmet_ls",
+        },
+        automatic_installation = true,
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      local servers = {
+        html = {},
+        cssls = {},
+        ts_ls = {},
+        jsonls = {},
+        emmet_ls = {
+          filetypes = { "html", "css", "scss", "javascript", "typescript", "vue" }
+        },
+      }
+
+      for server_name, server_config in pairs(servers) do
+        server_config.capabilities = capabilities
+        lspconfig[server_name].setup(server_config)
+      end
+    end
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      require("luasnip.loaders.from_vscode").lazy_load()
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        performance = {
+          max_view_entries = 15,
+        },
+      })
+    end
+  },
+
+  -- Status line
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'gruvbox',
+        },
+      })
+    end
+  },
+
+  -- Auto pairs
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup()
+    end
+  },
+
+  -- Comment plugin
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  },
+
+}, {
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen", 
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+-- Key mappings
+local keymap = vim.keymap.set
+
+-- General mappings
+keymap("n", "<C-s>", ":w<CR>", { desc = "Save file" })
+keymap("i", "<C-s>", "<Esc>:w<CR>a", { desc = "Save file in insert mode" })
+
+-- Neotree mappings
+keymap("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+
+-- Telescope mappings
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" })
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
+
+-- LSP mappings
+keymap("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+keymap("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+keymap("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+keymap("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format" })
+
+-- Copilot mappings
+keymap("i", "<C-g>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false, desc = "Accept Copilot" })
+keymap("i", "<C-\\>", 'copilot#Dismiss()', { expr = true, silent = true, desc = "Dismiss Copilot" })
+
+-- Buffer navigation
+keymap("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+keymap("n", "<leader>bp", ":bprev<CR>", { desc = "Previous buffer" })
+
+-- Clear search highlighting
+keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- Auto commands
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local last_pos = vim.fn.line("'\"")
+    if last_pos > 1 and last_pos <= vim.fn.line("$") then
+      vim.api.nvim_win_set_cursor(0, {last_pos, 0})
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
+
+-- LSP diagnostic configuration
+vim.diagnostic.config({
+  virtual_text = { prefix = "●" },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+  },
+})
