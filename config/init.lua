05d30844ffd35fbd1dedf5151fb86597f6df848b
Alkama Sudad <alkamasudad3@gmail.com>
2026-01-26 11:02:48 +0600

    updated capabilities

--- config/init.lua
+++ config/init.lua
@@ -225,8 +225,7 @@ require("lazy").setup({
       })
 
       
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-
+      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
       local servers = {
         html = {},
